58a95fda4f92df2ef1beac9af5587d5c4534dcae
arifur07 <arifur.rahman7@hotmail.com>
2023-01-15 09:06:57 +0530

    adding the monitoring stack into the cluster

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "awsEC2KopsInstance" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
+  key_name               = "Kops-Keypair"
   vpc_security_group_ids = ["sg-087b87c09faf36f43"]
   tags = {
     Name    = "bootstrap"
